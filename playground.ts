f9ff226d68ed7d731ce6a7ee7d3bd5c854a7e6f1
NRaykov <nediotenev@gmail.com>
2019-06-29 13:27:40 +0300

    Create custom complex types

--- playground.ts
+++ playground.ts
@@ -108,4 +108,17 @@ let complex: { data: number[], output: (all: boolean) => number[] } = {
         }
 };
 
+//Complex2 object custom complex types
+
+//create a custom type
+type Complex = { data: number[], output: (all: boolean) => number[] };
+
+let complex2: Complex = {
+    data: [100, 3.99, 10],
+    output: function (all: boolean): number[] {
+        return this.data;
+    }
+};
+
+
 
