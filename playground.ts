1b0969637165b2141e5a9462e5510cc7f7d6fc2c
NRaykov <nediotenev@gmail.com>
2019-06-29 13:22:20 +0300

    Complex object

--- playground.ts
+++ playground.ts
@@ -100,4 +100,12 @@ userData = {
 };
 
 
+//Complex object
+let complex: { data: number[], output: (all: boolean) => number[] } = {
+        data: [100, 3.99, 10],
+        output: function (all: boolean): number[] {
+            return this.data;
+        }
+};
+
 
